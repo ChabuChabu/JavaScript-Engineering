b4e4f0fbc708d303896e76fc5c4dc651305ec954
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-25 23:26:26 +0100

    arrow-functions

--- 02-functions/app.js
+++ 02-functions/app.js
@@ -14,9 +14,27 @@
 
 //Returning values
 
-const calcArea = function (radius) {
-  return 3.14 * radius ** 2;
-};
+// const calcArea = function (radius) {
+//   return 3.14 * radius ** 2;
+// };
+
+// const area = calcArea(5);
+// console.log(area);
+
+//Arrows functions
+
+const calcArea = (radius) => 3.14 * radius ** 2;
 
 const area = calcArea(5);
 console.log(area);
+
+const bill = (products, tax) => {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+};
+
+const total = bill([10, 15, 30], 0.2);
+console.log(total);
