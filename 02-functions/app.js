dbb2ce955ee057c029cafb253de5390738418024
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-25 22:57:39 +0100

    function expression

---
+++ 02-functions/app.js
@@ -0,0 +1,12 @@
+//function declaration
+// function greet() {
+//   console.log("Hello there!!");
+// }
+// greet();
+
+//function expression
+const speak = function (name, time) {
+  console.log(`good ${time} ${name}`);
+};
+
+speak("chabu", "morning");
