2fc7e0bd74f70d9966013e751133fce1ecb84ad0
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-25 08:32:22 +0100

    while loops

--- 01-controlFlow/app.js
+++ 01-controlFlow/app.js
@@ -6,11 +6,25 @@
 
 // console.log("Loop is finished");
 
-const names = ["shaun", "luigi", "mario"];
+// const names = ["shaun", "luigi", "mario"];
 
-for (let i = 0; i < names.length; i++) {
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
-}
+// for (let i = 0; i < names.length; i++) {
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
 
 /**************************************while loops ***********************************/
+const names = ["shaun", "luigi", "mario"];
+// let i = 0;
+
+// while (i < 5) {
+//   console.log("In while loop:", i);
+//   i++;
+// }
+
+let i = 0;
+
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
