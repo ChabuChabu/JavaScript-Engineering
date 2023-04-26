3a780e887bca17f69f20c5e2b8a4d88369db7ec4
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-26 11:28:34 +0100

    Math objects

--- 03-Objects/app.js
+++ 03-Objects/app.js
@@ -7,28 +7,48 @@
 
 // console.log(blogs);
 
-let user = {
-  name: "crystal",
-  age: 30,
-  email: "crystal@thenetninja.co,uk",
-  location: "berlin",
-  blogs: [
-    { title: "Why mac and cheese rules", likes: 30 },
-    { title: "10 things to make with marmite", likes: 50 },
-  ],
-  login: function () {
-    console.log("The user logged in");
-  },
-  logout: function () {
-    console.log("The user logged out");
-  },
-  logBlogs: function () {
-    //console.log(this);
-    console.log("This use has written the following blogs");
-    this.blogs.forEach((blog) => {
-      console.log(blog.title, blog.likes);
-    });
-  },
-};
+// let user = {
+//   name: "crystal",
+//   age: 30,
+//   email: "crystal@thenetninja.co,uk",
+//   location: "berlin",
+//   blogs: [
+//     { title: "Why mac and cheese rules", likes: 30 },
+//     { title: "10 things to make with marmite", likes: 50 },
+//   ],
+//   login: function () {
+//     console.log("The user logged in");
+//   },
+//   logout: function () {
+//     console.log("The user logged out");
+//   },
+//   logBlogs: function () {
+//     //console.log(this);
+//     console.log("This use has written the following blogs");
+//     this.blogs.forEach((blog) => {
+//       console.log(blog.title, blog.likes);
+//     });
+//   },
+// };
 
-user.logBlogs();
+// user.logBlogs();
+
+/***********************  Math Object ****************************/
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+//random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 5));
