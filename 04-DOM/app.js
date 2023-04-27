0001cbc3d7f47440bfa4a83472625ce06b9101f0
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-27 17:16:26 +0100

    changing content

--- 04-DOM/app.js
+++ 04-DOM/app.js
@@ -1,4 +1,25 @@
-const para = document.querySelectorAll("p");
-const errors = document.querySelectorAll(".error");
-console.log(errors);
-console.log(para[2]);
+const para = document.querySelector("p");
+
+//console.log(para.innerText);
+//para.innerText = "ninjas are awesome!";
+
+/****************** Changing the inner text ***********************/
+
+// const paras = document.querySelectorAll("p");
+// paras.forEach((para) => {
+//   console.log(para.innerText);
+//   para.innerText += "new text";
+// });
+
+/****************** Changing the html ***********************/
+
+const content = document.querySelector(".content");
+
+//console.log(content.innerHTML);
+// content.innerHTML = "<h2> This is a new </h2>";
+
+const people = ["mario", "luigi", "yoshi"];
+
+people.forEach((person) => {
+  content.innerHTML += `<p>${person}</p>`;
+});
