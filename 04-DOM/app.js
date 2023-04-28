6f59d2c8c6eb4d4c35e0f62fca8de05d1d91b19e
Chabu Chabu <chabu.chabu@episerver.com>
2023-04-28 12:42:33 +0100

    set attribute

--- 04-DOM/app.js
+++ 04-DOM/app.js
@@ -13,13 +13,24 @@ const para = document.querySelector("p");
 
 /****************** Changing the html ***********************/
 
-const content = document.querySelector(".content");
+// const content = document.querySelector(".content");
 
-//console.log(content.innerHTML);
-// content.innerHTML = "<h2> This is a new </h2>";
+// //console.log(content.innerHTML);
+// // content.innerHTML = "<h2> This is a new </h2>";
 
-const people = ["mario", "luigi", "yoshi"];
+// const people = ["mario", "luigi", "yoshi"];
 
-people.forEach((person) => {
-  content.innerHTML += `<p>${person}</p>`;
-});
+// people.forEach((person) => {
+//   content.innerHTML += `<p>${person}</p>`;
+// });
+
+/****************** setting attributes ***********************/
+const link = document.querySelector("a");
+
+console.log(link.getAttribute("href"));
+link.setAttribute("href", "https://www.thenetninja.co.uk");
+link.innerText = "The Net Ninja Website";
+const mssg = document.querySelector("p");
+console.log(mssg.getAttribute("class"));
+mssg.setAttribute("class", "success");
+mssg.setAttribute("style", "color: green;");
