27e0d17af92f13bf4e113f28aaffcec4ec9b2478
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-01 09:00:22 +0100

    adding and removing elements

--- 04-DOM/app.js
+++ 04-DOM/app.js
@@ -75,14 +75,28 @@
 // button.addEventListener("click", () => {
 //   console.log("You clicked me");
 // });
-
+const ul = document.querySelector("ul");
 const items = document.querySelectorAll("li");
+const button = document.querySelector("button");
+
+/*******add items to dom *******/
+button.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = "something new to do";
+  //ul.append(li);
+  ul.prepend(li);
+});
+
+// ul.remove();
 
 items.forEach((item) => {
   item.addEventListener("click", (e) => {
     // console.log("Item clicked");
     // console.log(e);
     // console.log(e.target);
-    e.target.style.textDecoration = "line-through";
+    // e.target.style.textDecoration = "line-through";
+
+    /*******remove of items from dom *******/
+    e.target.remove();
   });
 });
