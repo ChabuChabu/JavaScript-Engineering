5b53241bff738bf2b8788eb47565717e315c5f9d
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-02 13:33:19 +0100

    regex basics

--- 05-Forms/app.js
+++ 05-Forms/app.js
@@ -1,8 +1,19 @@
 const form = document.querySelector(".sign-up");
-//const username = document.querySelector("#username");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   //console.log(username.value);
   console.log(form.username.value);
 });
+
+//testing Regex
+const username = "shaunband";
+const pattern = /^[a-z]{6,}$/;
+
+let result = pattern.test(username);
+
+if (result) {
+  console.log("The regex test passed");
+} else {
+  console.log("The regex test failed!");
+}
