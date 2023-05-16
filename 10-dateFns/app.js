738fbf639cbc4eab10418a1f045126e7b76ceb48
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-16 16:51:40 +0100

    Heas=der added

--- 10-dateFns/app.js
+++ 10-dateFns/app.js
@@ -11,6 +11,10 @@ console.log(dateFns.format(now, "dddd Do,MMMM,YYYY"));
 
 //comparing dates
 
+const timestamp = 1675938474990;
+
+console.log(dateFns.distanceInWords(now, timestamp, { addSuffix: true }));
+
 const before = new Date("February 1 2019 12:00:00");
 
 console.log(dateFns.distanceInWords(now, before, { addSuffix: true }));
