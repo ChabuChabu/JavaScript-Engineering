60fb305cbbe4d1b44f7c8b1cae5c731c5b611112
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-16 13:34:02 +0100

    digital clock

--- 09-dateTimes/app.js
+++ 09-dateTimes/app.js
@@ -19,29 +19,51 @@
 
 /***********************  Timestamps ************************/
 
-const before = new Date("February 1 2019 7:30:59");
-const now = new Date();
+// const before = new Date("February 1 2019 7:30:59");
+// const now = new Date();
 
 // console.log(now.getTime(), before.getTime());
-const diff = now.getTime() - before.getTime();
-console.log(diff);
+// const diff = now.getTime() - before.getTime();
+// console.log(diff);
 
 //seconds
-const mins = Math.round(diff / 1000 / 60);
-console.log(mins);
+// const mins = Math.round(diff / 1000 / 60);
+// console.log(mins);
 
 //hours
-const hours = Math.round(mins / 60);
-console.log(hours);
+// const hours = Math.round(mins / 60);
+// console.log(hours);
 
 //days
-const days = Math.round(hours / 24);
-console.log(days);
+// const days = Math.round(hours / 24);
+// console.log(days);
 
-console.log(`The blog was written ${days} ago!`);
+// console.log(`The blog was written ${days} ago!`);
 
 //convering timestaps into date objects
 
-const timestamp = 1675938474990;
+// const timestamp = 1675938474990;
 
-console.log(new Date(timestamp));
+// console.log(new Date(timestamp));
+
+/***********************  Digital Clock ************************/
+
+const clock = document.querySelector(".clock");
+
+const tick = () => {
+  const now = new Date();
+
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  const html = `
+      <span>${h}</span> :
+      <span>${m}</span> :
+      <span>${s}</span> 
+  `;
+
+  clock.innerHTML = html;
+};
+
+setInterval(tick, 1000);
