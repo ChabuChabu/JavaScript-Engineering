7b2d596ad8d2751c6b907fc83f100644a40c5371
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-18 23:44:29 +0100

    promises basics

--- 11-Asyc/app.js
+++ 11-Asyc/app.js
@@ -1,25 +1,25 @@
 const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("could not fetch data", undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("error getting resource");
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
   });
-
-  request.open("GET", resource);
-  request.send();
 };
 
-getTodos("todos/luigi.json", (err, data) => {
-  console.log(data);
-  getTodos("todos/mario.json", (err, data) => {
-    console.log(data);
-    getTodos("todos/shaun.json", (err, data) => {
-      console.log(data);
-    });
+getTodos("todos/luigi.json")
+  .then((data) => {
+    console.log("promise resolved:", data);
+  })
+  .catch((err) => {
+    console.log("promise rejected", err);
   });
-});
