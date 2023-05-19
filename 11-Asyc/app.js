981df908f6e815481ddfcee97151125e4d963478
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-19 12:19:55 +0100

    promises

--- 11-Asyc/app.js
+++ 11-Asyc/app.js
@@ -18,7 +18,15 @@ const getTodos = (resource, callback) => {
 
 getTodos("todos/luigi.json")
   .then((data) => {
-    console.log("promise resolved:", data);
+    console.log("promise 1 resolved:", data);
+    return getTodos("todos/mario.json");
+  })
+  .then((data) => {
+    console.log("Promise 2 resolved", data);
+    return getTodos("todos/shaun.json");
+  })
+  .then((data) => {
+    console.log("Promise 3 resolved", data);
   })
   .catch((err) => {
     console.log("promise rejected", err);
