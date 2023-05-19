07144289b4b29516235843d7d54e7075b7c8cacf
Chabu Chabu <chabu.chabu@episerver.com>
2023-05-19 15:27:46 +0100

    async and await

--- 12-fetch/app.js
+++ 12-fetch/app.js
@@ -1,13 +1,20 @@
 //fetch api
+const getTodos = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  return data;
+};
 
-fetch("todos/luigi.json")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+getTodos().then((data) => console.log(data));
+
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
